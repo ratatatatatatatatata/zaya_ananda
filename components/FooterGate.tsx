f9d5d49d9908e1d512caa3cb908364da1957558e
buldruu <turuu.naranbaatar0711@gmail.com>
2026-08-28 16:35:58 +0800

    Нүүр хуудасны доод хэсэгт footer-ийг харуулах

--- components/FooterGate.tsx
+++ components/FooterGate.tsx
@@ -1,10 +1,5 @@
-"use client";
-
-import { usePathname } from "next/navigation";
 import { Footer } from "./Footer";
 
 export function FooterGate() {
-  const pathname = usePathname();
-  if (pathname === "/") return null;   // homepage = pure cinematic hero, no footer
   return <Footer />;
 }
